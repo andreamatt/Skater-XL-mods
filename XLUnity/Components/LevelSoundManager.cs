7a1fe83220553d29a7af8926ba84691743a96adb
Greg Amato <amatobahn@gmail.com>
2020-11-24 20:58:05 -0600

    First Pass at Material based sound tag system
    This sound tag system utilizes Serialized SO data representing mapping from material to tag index.
    
    Data is collected every frame from a raycast down from a negative vector from the board controller location. This hit location must be a mesh collider for the resulting quad to be represented as a material at its approximate location.
    
    Known Issues:
    - Have not tested lower resolution Mesh Colliders in place of the higer point count visual mesh.
    - Higher density meshes may take longer to iterate through the verticies to locate the triangle collected. An approach would be to precache ranges in which the materials are tied to. and instaead of iterating through triangles, do a sanity check for materials that belong in the range the vertex resides.

---
+++ XLUnity/Components/LevelSoundManager.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+using System.Linq;
+
+using UnityEngine;
+
+public class LevelSoundManager : MonoBehaviour
+{ 
+	public MaterialSoundTagCollection tagCollection;
+}
