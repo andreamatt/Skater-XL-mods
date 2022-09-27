c55dd61b10fa4dd94ef444fbfaa7fd9e25060e21
Roque Francisco <roquefsd@gmail.com>
2022-09-27 15:53:37 -0300

    remove unused references

--- SoundMod/SoundMod.cs
+++ SoundMod/SoundMod.cs
@@ -1,12 +1,8 @@
 using HarmonyLib;
-using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Text;
 using UnityEngine;
-using UnityEngine.Networking;
 using System.Linq;
-using ReplayEditor;
 using System.Text.RegularExpressions;
 
 namespace SoundMod
